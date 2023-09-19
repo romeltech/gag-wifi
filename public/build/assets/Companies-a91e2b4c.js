0c71ed3a501d1a9233634180c438d387f5961775
Romel Indemne <romel.i@gagroup.net>
2023-09-19 11:45:34 +0400

    removed build folder in gitignore

---
+++ public/build/assets/Companies-a91e2b4c.js
@@ -0,0 +1,1 @@
+import{c as s,w as a,v as t,o,a as e,y as c,j as n}from"./app-a7487ad6.js";import{_}from"./PageHeader-3716195d.js";const l=n("div",{class:"v-col-12"},null,-1),d={__name:"Companies",setup(r){return(i,m)=>(o(),s(t,null,{default:a(()=>[e(_,{title:"Companies"}),e(c,null,{default:a(()=>[l]),_:1})]),_:1}))}};export{d as default};
---
+++ public/build/assets/Departments-5fa5f615.js
@@ -0,0 +1,1 @@
+import{c as t,w as e,v as s,o,a,y as r,j as c}from"./app-a7487ad6.js";import{_ as n}from"./PageHeader-3716195d.js";const _=c("div",{class:"v-col-12"},null,-1),d={__name:"Departments",setup(l){return(i,m)=>(o(),t(s,null,{default:e(()=>[a(n,{title:"Departments"}),a(r,null,{default:e(()=>[_]),_:1})]),_:1}))}};export{d as default};
