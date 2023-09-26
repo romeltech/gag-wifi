8842af8e7cbba8eb2f44603569471dab9e4368d4
Romel Indemne <romel.i@gagroup.net>
2023-09-26 11:24:05 +0400

    added created_at

--- public/build/assets/Logs-050e782e.js
+++
@@ -1,1 +0,0 @@
-import{c as s,w as a,x as t,o,a as e,E as c,j as _}from"./app-337bfd4b.js";import{_ as l}from"./PageHeader-1c494705.js";const r=_("div",{class:"v-col-12"},null,-1),m={__name:"Logs",setup(n){return(i,u)=>(o(),s(t,null,{default:a(()=>[e(l,{title:"Logs"}),e(c,null,{default:a(()=>[r]),_:1})]),_:1}))}};export{m as default};
